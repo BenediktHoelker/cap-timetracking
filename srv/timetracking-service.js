81269a2eeec6202f637e08fac09194869e11349b
Benedikt Hölker <benedikt.hoelker@gmail.com>
2019-12-31 00:54:44 +0100

    Custom service Impl

--- srv/timetracking-service.js
+++ srv/timetracking-service.js
@@ -1,12 +1,18 @@
 module.exports = srv => {
-  srv.before("CREATE", "Projects", async ({ data, target, query }) => {
-    console.log(data);
-    console.log(target);
-    console.log(query);
+  const { Records } = cds.entities("my.timetracking.Records");
+
+  srv.before("DELETE", "Records", req => {
+    if (req.data.status !== "INITIAL") {
+      req.error("409", "The record status is not initial.");
+    }
   });
-  srv.before("CREATE", "Invoices", async ({ data, target, query }) => {
-    console.log(data);
-    console.log(target);
-    console.log(query);
+
+  srv.before("CREATE", "Invoices", async req => {
+    const tx = cds.transaction(req);
+    const invoice = req.data;
+
+    console.log(invoice);
+
+    return cds.read(Records);
   });
 };
