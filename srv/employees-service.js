8fd9d2415f414172c196a5e9f16d1bee35969186
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2020-04-22 18:03:59 +0200

    Make sure no duplicate projects are allocated to an employee

--- srv/employees-service.js
+++ srv/employees-service.js
@@ -1,8 +1,20 @@
 const cds = require("@sap/cds");
-const { Leaves } = cds.entities;
+const { EmployeesToProjects } = cds.entities;
 
 module.exports = (srv) => {
   srv.before(["CREATE", "UPDATE"], "Employees", (req) => {
+    const projectMembers = req.data.projects;
+
+    let seen = new Set();
+    // https://stackoverflow.com/questions/30735465/how-can-i-check-if-the-array-of-objects-have-duplicate-property-values
+    const hasDuplicateProjects = projectMembers.some(
+      (member) => seen.size === seen.add(member.project_ID).size
+    );
+
+    if (hasDuplicateProjects) {
+      return req.error(410, "Duplicate projects are not allowed.");
+    }
+
     req.data.leaves.forEach(
       (leave) => (leave.daysOfLeave = getDaysBetween(leave))
     );
@@ -10,16 +22,6 @@ module.exports = (srv) => {
     req.data.travels.forEach(
       (travel) => (travel.daysOfTravel = getDaysBetween(travel))
     );
-
-    // req.data.daysOfLeave = req.data.leaves.reduce(
-    //   (acc, curr) => acc + curr.daysOfLeave,
-    //   0
-    // );
-
-    // req.data.daysOfTravel = req.data.travels.reduce(
-    //   (acc, curr) => acc + curr.daysOfTravel,
-    //   0
-    // );
   });
 };
 
